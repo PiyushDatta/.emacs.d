7fbfcd86c556988773589f08d086617ac0735d35
Piyush Datta <piyushdatta@Dev-1623.local>
2019-12-03 17:24:42 -0500

    Added a new function for untabbing as well, and replaced tab function for a better function

--- init.el
+++ init.el
@@ -337,13 +337,51 @@
 ;; Open/add a new projectile through treemacs
 (define-key global-map (kbd "C-x p ") 'treemacs-add-project-to-workspace)
 
-;; Make tab, always insert tab
-(defun my-insert-tab-char ()
-  "Insert a tab char. (ASCII 9, \t)"
-  (interactive)
-  (insert "\t"))	
+;; Make tab, always insert tab and untab with shift+tab as well, set to 4-spaces right now
+(defun indent-region-custom(numSpaces)
+    (progn
+        ; default to start and end of current line
+        (setq regionStart (line-beginning-position))
+        (setq regionEnd (line-end-position))
 
-(global-set-key (kbd "TAB") 'my-insert-tab-char) ; same as Ctrl+i
+        ; if there's a selection, use that instead of the current line
+        (when (use-region-p)
+            (setq regionStart (region-beginning))
+            (setq regionEnd (region-end))
+        )
+
+        (save-excursion ; restore the position afterwards
+            (goto-char regionStart) ; go to the start of region
+            (setq start (line-beginning-position)) ; save the start of the line
+            (goto-char regionEnd) ; go to the end of region
+            (setq end (line-end-position)) ; save the end of the line
+
+            (indent-rigidly start end numSpaces) ; indent between start and end
+            (setq deactivate-mark nil) ; restore the selected region
+        )
+    )
+)
+
+(defun untab-region (N)
+    (interactive "p")
+    (indent-region-custom -4)
+)
+
+(defun tab-region (N)
+    (interactive "p")
+    (if (active-minibuffer-window)
+        (minibuffer-complete)    ; tab is pressed in minibuffer window -> do completion
+    ; else
+    (if (string= (buffer-name) "*shell*")
+        (comint-dynamic-complete) ; in a shell, use tab completion
+    ; else
+    (if (use-region-p)    ; tab is pressed is any other buffer -> execute with space insertion
+        (indent-region-custom 4) ; region was selected, call indent-region
+        (insert "    ") ; else insert four spaces as expected
+    )))
+)
+(global-set-key (kbd "<S-tab>") 'untab-region)
+(global-set-key (kbd "<tab>") 'tab-region)
 
 ;; comment or uncomment region/line
 (defun comment-or-uncomment-region-or-line ()
