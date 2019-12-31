c12ab077d451571035a55cb9d63a28cc19757e58
PiyushDatta <piyushdattaca@gmail.com>
2019-12-31 17:29:44 -0500

    Adding my custom-set-variables

---
+++ custom-set-variables.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flymake-google-cpplint-command
+   "C:/Users/PD/AppData/Local/Programs/Python/Python36/Scripts/cpplint")
+ '(flymake-google-cpplint-filter "--filter=-whitespace/line_length,-build")
+ '(flymake-google-cpplint-verbose "--verbose=0")
+ '(package-selected-packages
+   (quote
+	(format-all flymake-google-cpplint flymake-cursor google-c-style flymake-google-ccplint irony-eldoc flycheck-irony company-irony company-irony-c-headers irony dumb-jump swiper treemacs-magit treemacs-projectile centaur-tabs yasnippet-snippets web-mode company-lsp lsp-java lsp-mode magit flx-ido ido-vertical-mode solaire-mode projectile use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
