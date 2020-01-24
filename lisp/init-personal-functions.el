19826ba85d2e758887f50a79b15d173f23a353a1
PiyushDatta <piyushdattaca@gmail.com>
2020-01-23 22:00:50 -0500

    Added a python run command so we can substitude a virtual env instead of just passing in python command

--- lisp/init-personal-functions.el
+++ lisp/init-personal-functions.el
@@ -143,7 +143,9 @@
   "Compile and run python code."
   (interactive)
   (setq current-function-time (current-time))
-  (setq compile-shell-command (format "python '%s'" curr-file-full-name))
+  ;; (setq python-run-command "test_venv/bin/python")
+  (setq python-run-command "python")
+  (setq compile-shell-command (format "%s '%s'" python-run-command curr-file-full-name))
 
   ;; Compile and execute the file
   (save-buffer curr-file-full-name)
